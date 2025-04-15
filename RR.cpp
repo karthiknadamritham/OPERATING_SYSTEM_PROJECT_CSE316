5b14b29ec44a34388d7d54389c4bdd45e9974332
KARTHIK S N <145195261+karthiknadamritham@users.noreply.github.com>
2025-04-15 21:34:42 +0530

    Update RR.cpp

--- RR.cpp
+++ RR.cpp
@@ -20,4 +20,39 @@ int main() {
         p[i].remainingTime = p[i].burstTime;
         p[i].waitingTime = 0;
     }
+
+    int time = 0, completed = 0;
+    while (completed < n) {
+        bool done = true;
+        for (int i = 0; i < n; i++) {
+            if (p[i].remainingTime > 0) {
+                done = false;
+                if (p[i].remainingTime > timeQuantum) {
+                    time += timeQuantum;
+                    p[i].remainingTime -= timeQuantum;
+                } else {
+                    time += p[i].remainingTime;
+                    p[i].waitingTime = time - p[i].burstTime;
+                    p[i].turnaroundTime = time;
+                    p[i].remainingTime = 0;
+                    completed++;
+                }
+            }
+        }
+        if (done) break;
+    }
+
+    float totalWaitingTime = 0, totalTurnaroundTime = 0;
+    cout << "\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n";
+    for (int i = 0; i < n; i++) {
+        totalWaitingTime += p[i].waitingTime;
+        totalTurnaroundTime += p[i].turnaroundTime;
+        cout << "P" << p[i].id << "\t" << p[i].burstTime << "\t\t"
+             << p[i].waitingTime << "\t\t" << p[i].turnaroundTime << endl;
+    }
+
+    cout << "\nAverage Waiting Time: " << (totalWaitingTime / n);
+    cout << "\nAverage Turnaround Time: " << (totalTurnaroundTime / n) << endl;
+
+    return 0;
 }
